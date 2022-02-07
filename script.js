dde32de8024c6c3f83a7056bc127fe04ac276006
hotarakit <97969246+hotarakit@users.noreply.github.com>
2022-02-07 11:10:46 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".answer").click(function() {
+    let container = $("provider").val();
+    $(".output1").text("Using AT&T is very reccommended, since both premium plans are at the same price, you'll get more benefits if you use AT&T Unlimited Elite Plan which is better than the T-Mobile Magenta Max");
+    console.log(container);
+    
+    
+        
+
+
+});
